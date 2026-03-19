7743e75530269462748c95cd0c86f2576ae91829
Michael du Plessis <mdp0101@gmail.com>
2026-03-19 15:41:25 +0200

    used kiro to write tests for termination module

--- src/termination.rs
+++ src/termination.rs
@@ -21,3 +21,30 @@ impl<G, F> TerminationCondition<G, F> for MaxGenerations {
         state.generation() >= self.0
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::core::{population::Population, state::State};
+
+    #[test]
+    fn terminates_at_max_generation() {
+        let tc = MaxGenerations::new(10);
+        let state = State::<(), ()>::new(Population::new(), 10);
+        assert!(tc.should_terminate(&state));
+    }
+
+    #[test]
+    fn terminates_past_max_generation() {
+        let tc = MaxGenerations::new(10);
+        let state = State::<(), ()>::new(Population::new(), 15);
+        assert!(tc.should_terminate(&state));
+    }
+
+    #[test]
+    fn does_not_terminate_before_max() {
+        let tc = MaxGenerations::new(10);
+        let state = State::<(), ()>::new(Population::new(), 9);
+        assert!(!tc.should_terminate(&state));
+    }
+}
